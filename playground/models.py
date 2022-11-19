4969f42d7c5adef1ba0604a6f5fa4c6e791c489d
Gilbert <gilbert.kwame.adu@outlook.com>
2022-11-19 14:36:17 +0200

    updates on models from joni advice

--- playground/models.py
+++ playground/models.py
@@ -2,83 +2,74 @@ from django.db import models
 
 
 class MultimodalData(models.Model):
-    #image files for both
-    preop_mri = models.CharField(max_length=200)
-    postop_mri = models.CharField(max_length=200)
-
-    #text field
-    biopsy = models.CharField(max_length=200)
+    preop_mri = models.ImageField(blank=True, null=True, upload_to='media')
+    postop_mri = models.ImageField(blank=True, null=True, upload_to='media')
+    biopsy = models.TextField(max_length=200)
 
     def __str__(self):
         return self.biopsy
 
 class RgbImage(models.Model):
     #todo add image field
-
     #as a mask image field
-    anatomical_annot = models.CharField(max_length=200)
+    anatomical_annot = models.ImageField(blank=True, null=True, upload_to='media')
 
     def __str__(self):
         return self.anatomical_annot
+
+class Patient(models.Model):
+    patientID = models.CharField(max_length=20, default="1")
+
+    #Has being worked on
+    #works backs to the patient same as the multimodal data
+    #hyper_spectral_image = models.ForeignKey(HyperSpectralImage, on_delete = models.CASCADE)
+    #multi_modal_data = models.ForeignKey(MultimodalData, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.patientID
 
 class HyperSpectralImage(models.Model):
     #todo how to get the individual band from tiff file
 
     hyperspec_image = models.CharField(max_length=200)
-    #text field
-    imaging_data = models.CharField(max_length=200)
-    #eg both int
-    resolutionHeight = models.CharField(max_length=200)
-    resolutionWidth = models.CharField(max_length=200)
-
-    #band and frames + integer
-    bands = models.CharField(max_length=200)
-    frames = models.CharField(max_length=200)
-    rgb_image = models.OneToOneField(RgbImage, on_delete = models.CASCADE)
-
+    imaging_data = models.TextField(max_length=200)
+    resolutionHeight = models.IntegerField(null=True)
+    resolutionWidth = models.IntegerField(null=True)
+    bands = models.PositiveIntegerField(null=True)
+    frames = models.PositiveIntegerField(null=True)
+    rgb_image = models.OneToOneField(RgbImage, null=True, on_delete = models.SET_NULL)
+    patient = models.OneToOneField(Patient, null=True, on_delete = models.SET_NULL)
+    multi_modal_Data = models.OneToOneField(MultimodalData, null=True, on_delete = models.SET_NULL)
     actual_image = models.ImageField(blank=True, null=True, upload_to='media')
 
     def __str__(self):
         return self.hyperspec_image
     
-class Patient(models.Model):
-    #age = models.IntegerField()
-    patientID = models.CharField(max_length=20, default="1")
-    #gender = models.CharField(max_length=1)
-
-    #works backs to the patient same as the multimodal data
-    hyper_spectral_image = models.ForeignKey(HyperSpectralImage, on_delete = models.CASCADE)
-    multi_modal_data = models.ForeignKey(MultimodalData, on_delete = models.CASCADE)
-
+class HsiSoftware(models.Model):
+    name = models.CharField(max_length=200, null=True)
+    #Has being worked on
+    #analysis_method = models.CharField(max_length=200)
+    #analysis_duration = models.CharField(max_length=200)
+    #works backwards to the image table
+    #imaging = models.ForeignKey(Imaging, null = True, on_delete = models.CASCADE)
     def __str__(self):
-        return self.patientID
+         return self.name
 
 class Imaging(models.Model):
     acquisition_type = models.CharField(max_length=200)
     light_intensity = models.CharField(max_length=200)
     light_source = models.CharField(max_length=200)
     device_name = models.CharField(max_length=200)
-
-    analysis_method = models.CharField(max_length=200)
-    analysis_duration = models.CharField(max_length=200)
-
+    analysis_method = models.CharField(max_length=200, null=True)
+    analysis_duration = models.CharField(max_length=200, null=True)
+    hsi_software = models.OneToOneField(HsiSoftware, null=True, on_delete = models.SET_NULL)
     hyper_spectral_image = models.ForeignKey(HyperSpectralImage, null = True, on_delete = models.CASCADE)
 
     def __str__(self):
         return  self.device_name
 
-class HsiSoftware(models.Model):
-    # analysis_method = models.CharField(max_length=200)
-    # analysis_duration = models.CharField(max_length=200)
-
-    #works backwards to the image table
-    imaging = models.ForeignKey(Imaging, null = True, on_delete = models.CASCADE)
-
-    def __str__(self):
-        return self.analysis_method
-
+# for more delibration on how to use this model with the team based on saving
 class ClassFeature(models.Model):
-    # for more delibration on how to use this model with the team based on saving
     class_feature = models.CharField(max_length=200)
 
     def __str__(self):
@@ -113,7 +104,7 @@ class Mask(models.Model):
 class Annotation(models.Model):
     annot_image = models.CharField(max_length=200)
     mask = models.ForeignKey(Mask, on_delete = models.CASCADE)
-    hyper_spectral_image = models.OneToOneField(HyperSpectralImage, on_delete = models.CASCADE)
+    hyper_spectral_image = models.OneToOneField(HyperSpectralImage, null=True, on_delete = models.SET_NULL)
 
 
 
